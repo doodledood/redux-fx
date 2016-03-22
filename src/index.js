f09f44c0f9f6bc6f268d8fc37680b36b2b23bbc5
doodledood <aviram.kofman@gmail.com>
2016-03-22 20:29:17 +0200

    support multiple arguments to effect

--- src/index.js
+++ src/index.js
@@ -93,5 +93,8 @@ export function fx(effect, ...args) {
 }
 
 function defaultEffectsRunner(effects, dispatch, getState) {
-  effects.forEach(effect => effect.func(effect.args)(dispatch, getState));
+  effects.forEach(effect => {
+    const funcWithArgs = Array.isArray(effect.args) ? effect.func(...effect.args) : effect.func(effect.args);
+    funcWithArgs(dispatch, getState);
+  });
 }
