4127570b51da7b948145b5bd17bcdd0d84ce6b07
doodledood <aviram.kofman@gmail.com>
2016-03-23 20:46:27 +0200

    add a nested example

---
+++ examples/nested.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { fx, mapFx, enhanceStoreWithEffects } from 'react-fx'
+
+const App = connect({state, dispatch} => {
+  return (
+    <div>
+      <Counter id="left" count={state.left} dispatch={tag(dispatch, "LEFT")} />
+      <Counter id="right" count={state.right} dispatch={tag(dispatch, "RIGHT")} />
+    </div>
+  );
+});
+
+const Counter = ({count, dispatch} => {
+  return (
+    <div>
+      <button onClick={() => dispatch({type:"INCREMENT"})}>+</button>
+      <div>{count}</div>
+      <button onClick={() => dispatch({type:"DECREMENT"})}>-</button>
+    </div>
+  );
+});
+
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "RIGHT":
+      const [rightCounterState, rightCounterEffect] = counterReducer(state.right, action.counterAction);
+      return [
+              {
+                right: rightCounterState,
+                left: state.left
+              },
+              mapFx(counterAction => {type: "RIGHT", counterAction}, rightCounterEffect)
+            ];
+    case "LEFT":
+      const [leftCounterState, leftCounterEffect] = counterReducer(state.left, action.counterAction);
+      return [
+              {
+                right: state.right,
+                left: leftCounterState
+              },
+              mapFx(counterAction => {type: "LEFT", counterAction}, leftCounterEffect)
+            ];
+    default:
+      return state;
+  }
+};
+
+const incrementWithDelay = seconds => dispatch => setTimeout(() => dispatch({type: "INCREMENT"}), seconds * 1000);
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT_DELAY":
+      return [state, fx(incrementWithDelay, 1)];
+    case "INCREMENT":
+      return {count: state.count + 1};
+    case "DECREMENT":
+      return {count: state.count - 1};
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, initialState, enhanceStoreWithEffects);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('app')
+);
