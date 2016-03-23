5a0f49bee0b4dd4a91f873d50da9aac939585b28
doodledood <aviram.kofman@gmail.com>
2016-03-24 00:25:59 +0200

    support nested batched effects

--- src/defaultEffectsRunner.js
+++ src/defaultEffectsRunner.js
@@ -5,9 +5,14 @@ export function defaultEffectsRunner(effect, dispatch, getState) {
     return;
   }
 
+  runEffect(effect, dispatch, getState);
+}
+
+export function runEffect(effect, dispatch, getState) {
   switch (effect.type) {
     case EffectTypes.FUNC:
-      runEffect(effect, dispatch, getState);
+      const funcWithArgs = Array.isArray(effect.args) ? effect.func(...effect.args) : effect.func(effect.args);
+      funcWithArgs(dispatchWithMap(effect.actionMapper, dispatch), getState);
       return;
     case EffectTypes.BATCH:
       effect.subEffects.forEach(subEffect => runEffect(subEffect, dispatch, getState));
@@ -15,11 +20,6 @@ export function defaultEffectsRunner(effect, dispatch, getState) {
     default:
       return;
   }
-}
-
-export function runEffect(effect, dispatch, getState) {
-  const funcWithArgs = Array.isArray(effect.args) ? effect.func(...effect.args) : effect.func(effect.args);
-  funcWithArgs(dispatchWithMap(effect.actionMapper, dispatch), getState);
 }
 
 export function dispatchWithMap(mapFunc, dispatch) {
